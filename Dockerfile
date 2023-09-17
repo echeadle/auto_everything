b15c38fbc470a307d214eb3db9c0afdbbfad104c
echeadle <echeadle@gmail.com>
2023-09-17 15:29:00 -0600

    initial commit after sep 17 2023 restart

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-alpine3.13
+FROM ubuntu:latest
 #FROM python:3.9
 LABEL maintainer="edward.cheadle8@gmail.com"
 
@@ -9,18 +9,14 @@ COPY ./requirements.dev.txt /tmp/requirements.dev.txt
 COPY ./scripts /scripts
 WORKDIR /scripts
 
-RUN python -m venv /py && \
+RUN apt-get install python  && \
+    python3 -m venv /py && \
     /py/bin/pip install --upgrade pip && \
-    apk add --update --no-cache postgresql-client jpeg-dev \
-        chromium chromium-chromedriver && \
-    apk add --update --no-cache --virtual .tmp-build-deps \
-        build-base postgresql-dev musl-dev zlib zlib-dev linux-headers && \
     /py/bin/pip install -r /tmp/requirements.txt && \
     if [ $DEV = "true" ]; \
       then  /py/bin/pip install -r /tmp/requirements.dev.txt ; \
     fi && \
     rm -rf /tmp && \
-    apk del .tmp-build-deps && \
     adduser \
         --disabled-password \
         --no-create-home \
--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-
+#from selenium.webdriver.common.by import By
 
 def get_driver():
     """ Set up Selenium webdriver for Chrome"""
+    #options = webdriver.FirefoxOptions()
     options = webdriver.ChromeOptions()
     options.add_argument("disable_infobars")
     options.add_argument("start-maximized")
@@ -10,9 +11,9 @@ def get_driver():
     options.add_argument("no-sandbox")
     options.add_experimental_option("excludeSwitches",
                                     ["enable-automation"])
+
     options.add_argument(
                          "disable-blink-features=AutomationControlled")
-
     driver = webdriver.Chrome(options=options)
     return driver
 
@@ -20,8 +21,8 @@ def main():
     """Main script"""
     driver = get_driver()
     driver.get("http://automated.pythonanywhere.com")
-    element = driver.fine_element_by_xpath("/html/body/div[1]/div/h1[1]")
-    print(element)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+    print(element.text)
 
 if __name__ == "__main__":
     main()
